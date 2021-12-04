45f63057242e88d0a9da854b9b78868a7c85acfb
acmon <acmon.soares@gmail.com>
2021-12-04 14:55:12 -0300

    add datetime field to model

--- app/models/Event.py
+++ app/models/Event.py
@@ -1,8 +1,10 @@
 from typing import Optional
 from uuid import UUID, uuid4
 from pydantic import BaseModel
+from datetime import datetime
 
 
 class Event(BaseModel):
     id: Optional[UUID] = uuid4()
     avenue: str
+    datetime: datetime
